56112d36b055190dc32ef5058aed016bef3773f7
Trevor Hackett <1577822+yarrgh@users.noreply.github.com>
2020-12-21 16:35:01 -0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -7,34 +7,35 @@ using System.IO;
 
 namespace ApiGateway
 {
-    class Program
+  class Program
+  {
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            new WebHostBuilder()
-               .UseKestrel()
-               .UseContentRoot(Directory.GetCurrentDirectory())
-               .ConfigureAppConfiguration((hostingContext, config) =>
-               {
-                   config
-                       .SetBasePath(hostingContext.HostingEnvironment.ContentRootPath)
-                       .AddJsonFile("appsettings.json", false, true)
-                       .AddJsonFile($"appsettings.{hostingContext.HostingEnvironment.EnvironmentName}.json", true, true)
-                       .AddEnvironmentVariables();
+      new WebHostBuilder()
+         .UseKestrel()
+         .UseContentRoot(Directory.GetCurrentDirectory())
+         .ConfigureAppConfiguration((hostingContext, config) =>
+         {
+           config
+                     .SetBasePath(hostingContext.HostingEnvironment.ContentRootPath)
+                     .AddJsonFile("appsettings.json", false, true)
+                     .AddJsonFile($"appsettings.{hostingContext.HostingEnvironment.EnvironmentName}.json", true, true)
+                     .AddEnvironmentVariables();
+         })
+         .ConfigureServices(s =>
+         {
+           s.AddOcelot();
+         })
+         .ConfigureLogging((hostingContext, logging) =>
+         {
+                 //add your logging
                })
-               .ConfigureServices(s => {
-                   s.AddOcelot();
-               })
-               .ConfigureLogging((hostingContext, logging) =>
-               {
-                   //add your logging
-               })
-               .Configure(app =>
-               {
-                   app.UseOcelot();
-               })
-               .Build()
-               .Run();
-        }
+         .Configure(app =>
+         {
+           app.UseOcelot();
+         })
+         .Build()
+         .Run();
     }
+  }
 }
