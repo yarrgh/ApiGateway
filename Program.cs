28567a796d8d41f2d1ca34d0797e4045f04cb7b2
Trevor Hackett <1577822+yarrgh@users.noreply.github.com>
2020-12-21 16:19:51 -0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+using System;
+using System.IO;
+
+namespace ApiGateway
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new WebHostBuilder()
+               .UseKestrel()
+               .UseContentRoot(Directory.GetCurrentDirectory())
+               .ConfigureAppConfiguration((hostingContext, config) =>
+               {
+                   config
+                       .SetBasePath(hostingContext.HostingEnvironment.ContentRootPath)
+                       .AddJsonFile("appsettings.json", false, true)
+                       .AddJsonFile($"appsettings.{hostingContext.HostingEnvironment.EnvironmentName}.json", true, true)
+                       .AddEnvironmentVariables();
+               })
+               .ConfigureServices(s => {
+                   s.AddOcelot();
+               })
+               .ConfigureLogging((hostingContext, logging) =>
+               {
+                   //add your logging
+               })
+               .Configure(app =>
+               {
+                   app.UseOcelot();
+               })
+               .Build()
+               .Run();
+        }
+    }
+}
